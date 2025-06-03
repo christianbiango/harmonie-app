4e2c084588f2b6546717ee253fc9cbaf53949967
Nohoarii FAMIBELLE <famibellenohoarii@hotmail.com>
2025-06-03 16:12:57 +0200

    Commit 1 - Add validation of registration form with zod [Not complete]

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -9,6 +9,9 @@ export default async function Signup(props: {
   searchParams: Promise<Message>;
 }) {
   const searchParams = await props.searchParams;
+
+  console.log("error" in searchParams);
+
   if ("message" in searchParams) {
     return (
       <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
@@ -30,6 +33,7 @@ export default async function Signup(props: {
         <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
           <Label htmlFor="email">Email</Label>
           <Input name="email" placeholder="johndoe@gmail.com" required />
+
           <Label htmlFor="password">Mot de passe</Label>
           <Input
             type="password"
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -24,6 +24,9 @@ export const createClient = async () => {
           }
         },
       },
-    },
+    }
   );
 };
+
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
