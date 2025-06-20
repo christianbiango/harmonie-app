7d3932a385c9fadc2bb40d43c42d1bfcd2b993dc
Nohoarii FAMIBELLE <famibellenohoarii@hotmail.com>
2025-06-20 14:33:57 +0200

    feat(offer-details) : Add offer details page

---
+++ app/protected/sejours/sejour-detail/page.tsx
@@ -0,0 +1,9 @@
+// app/offres/[id]/page.tsx
+export default async function OfferDetailsPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold">Mios</h1>
+      <h2 className="mt-4">Informations sur la commune</h2>
+    </main>
+  );
+}
