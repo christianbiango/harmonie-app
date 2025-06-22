68a60d0fd052972d871cc9dde374387111601ee9
christianbiango <biangojunior@gmail.com>
2025-06-22 20:37:13 +0200

    feat(home): add blue circle to 'pleinement' text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+"use client";
+
+import { BlueButton } from "@/components/BlueButtton";
+import DestinationDetails from "@/components/DestinationDetails";
+import DestinationSearch from "@/components/HomePage/DestinationSearch";
+import Header from "@/components/HomePage/Header";
+import NephosCard from "@/components/NephosCard";
+import { SearchBar } from "@/components/search/SearchBar";
+import { SearchResults } from "@/components/search/SearchResults";
+import { useState } from "react";
+
+interface SearchResult {
+  id: number;
+  name: string;
+  address: string;
+  description: string;
+}
+
 export default function Home() {
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+
   return (
     <>
-      <main className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        Nephos
+      <Header />
+      <main className="p-4">
+        <DestinationSearch />
       </main>
     </>
   );
