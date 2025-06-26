9b844bdb9ffcf2f666a576715c79ed6b8684749b
christianbiango <biangojunior@gmail.com>
2025-06-26 07:06:56 +0200

    style(favorite.Page): improve responsive

--- app/app/favoris/page.tsx
+++ app/app/favoris/page.tsx
@@ -2,16 +2,32 @@ import { FavoriteCard } from "@/components/HomePage/FavoriteCard";
 
 export default function FavorisPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#FFFBF5] p-4">
-      <FavoriteCard
-        title="Vigan"
-        subtitle="Commune du Sud"
-        tags={["En forêt", "Nature", "Lac"]}
-        image="/images/homepage/church.png"
-        population="3 786 habitants"
-        distance="À 42min de chez vous"
-        activity="Escalade en nature"
-      />
-    </div>
+    <>
+      <section className="mb-16">
+        <h1 className="text-2xl md:text-3xl font-bold text-center mb-4">
+          Favoris
+        </h1>
+        <div className="flex flex-col sm:flex-row items-center justify-center p-4 gap-8">
+          <FavoriteCard
+            title="Vigan"
+            subtitle="Commune du Sud"
+            tags={["En forêt", "Nature", "Lac"]}
+            image="/images/homepage/church.png"
+            population="3 786 habitants"
+            distance="À 42min de chez vous"
+            activity="Escalade en nature"
+          />
+          <FavoriteCard
+            title="Vigan"
+            subtitle="Commune du Sud"
+            tags={["En forêt", "Nature", "Lac"]}
+            image="/images/homepage/church.png"
+            population="3 786 habitants"
+            distance="À 42min de chez vous"
+            activity="Escalade en nature"
+          />
+        </div>
+      </section>
+    </>
   );
 }
