c1921d23c592b2ee70cd047cbe63b0bce82f776c
christianbiango <biangojunior@gmail.com>
2025-06-26 04:40:45 +0200

    feat(detail.Page): connect to supabase and book an offer

---
+++ app/(actions)/holidays.ts
@@ -0,0 +1,108 @@
+"use server";
+
+import { formatDateToLocalYYYYMMDD } from "@/utils/dates";
+import { createClient } from "@/utils/supabase/server";
+
+export const fetchHolidayAction = async (
+  column: string,
+  value: string | number
+) => {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("holidays_offers")
+    .select(
+      `
+      *,
+      cities(id, name, population, distance_from_doctor, holiday_type, region),
+      holidays_comments(
+        id,
+        comment,
+        created_at,
+        doctors_public(
+          id,
+          last_name
+        )
+      ),
+      holiday_offer_similarities_id_holidays_offers_fkey(
+        id_similar_holiday(
+          image_url,
+          image_alt,
+          name,
+          main_category
+        )
+    ),
+    offers_availability(date, available)
+    `
+    )
+    .eq(column, value)
+    .single();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (data?.holidays_comments) {
+    data.holidays_comments = data.holidays_comments.sort(
+      (a: any, b: any) =>
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+    )[0];
+  }
+
+  return data;
+};
+
+export async function insertBooking(
+  selectedDates: Date[],
+  offerId: string,
+  userId: string
+) {
+  const supabase = await createClient();
+
+  const datesStr = selectedDates.map(formatDateToLocalYYYYMMDD);
+
+  for (const date of datesStr) {
+    const { error: insertError } = await supabase
+      .from("offers_bookings")
+      .insert([
+        {
+          id_holidays_offers: offerId,
+          id_doctors: userId,
+          date,
+          status: "En attente de validation",
+        },
+      ]);
+
+    if (insertError) {
+      throw new Error(insertError.message);
+    }
+
+    const { error: updateError } = await supabase
+      .from("offers_availability")
+      .update({ available: false })
+      .eq("id_holidays_offers", offerId)
+      .eq("date", date);
+
+    if (updateError) {
+      throw new Error(updateError.message);
+    }
+  }
+
+  return { success: true };
+}
+
+// Fetch all recommended holidays for a user, with offer details
+export const fetchUserRecommendedOffers = async (userId: string) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("recommanded_holidays")
+    .select("id_holidays_offers, holidays_offers (id, name, image_url)")
+    .eq("id_doctors", userId);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  // Flatten the result to just the holidays_offers
+  return (data || []).map((row: any) => row.holidays_offers);
+};
